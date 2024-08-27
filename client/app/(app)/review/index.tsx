fd66b4f132dcd8b16af3ae84ae2f539142570e72
Max London <grahamluk66@gmail.com>
2024-08-28 00:28:43 +0100

    Used forward ref to use add post in header

--- client/app/(app)/review/index.tsx
+++ client/app/(app)/review/index.tsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
 import NewReview from "@/components/Review/newReview";
 import { Stack } from "expo-router";
 import NewReviewHeader from "@/components/headers/newReviewHeader";
 
+interface NewReviewRef {
+  submit: () => void;
+}
+
 const NewReviewPage = () => {
+  const newReviewRef = useRef<NewReviewRef>();
+
+  const submitReview = async () => {
+    const submitAction = newReviewRef?.current;
+    if (submitAction) {
+      submitAction.submit();
+    }
+  };
+
   return (
     <>
       <Stack.Screen
         options={{
-          header: () => <NewReviewHeader />,
+          header: () => <NewReviewHeader submit={submitReview} />,
         }}
       />
-      <NewReview />
+      <NewReview ref={newReviewRef} />
     </>
   );
 };
--- client/components/headers/newReviewHeader.tsx
+++ client/components/headers/newReviewHeader.tsx
@@ -1,16 +1,22 @@
 import React from "react";
-import { Text, Image } from "react-native";
+import { Text, Image, Pressable } from "react-native";
 import HeaderContainer from ".";
 import { Link } from "expo-router";
 
-const NewReviewHeader = () => {
+interface NewReviewHeaderProps {
+  submit: () => void;
+}
+
+const NewReviewHeader = ({ submit }: NewReviewHeaderProps) => {
   return (
     <HeaderContainer>
       <Link href="/">
         <Image source={require("../../assets/icons/cancel.svg")} />
       </Link>
       <Text>New Post</Text>
-      <Image source={require("../../assets/icons/plus.svg")} />
+      <Pressable onPress={submit}>
+        <Image source={require("../../assets/icons/plus.svg")} />
+      </Pressable>
     </HeaderContainer>
   );
 };
